929466f5835114bc3e8144edbfcb31f1a86e91d2
Sam Potts <sam.expression@gmail.com>
2025-08-11 09:28:15 +0000

    I would like to first of al change the font to `Quicksand` across the bo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Quicksand', 'sans-serif'],
+        headline: ['Quicksand', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
