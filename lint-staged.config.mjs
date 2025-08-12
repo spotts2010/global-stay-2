da5aa793a331f2ca2d53f9f8aac09b1d7815c785
Sam Potts <sam.expression@gmail.com>
2025-08-12 02:18:39 +0000

    Speed up pre-commit hooks

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,5 +1,8 @@
-// lint-staged.config.js
+// lint-staged.config.mjs
 export default {
-  '**/*.{js,mjs,cjs}': ['eslint --fix', 'prettier --write'],
-  '**/*.{json,md,css}': ['prettier --write'],
+  '**/*.{js,mjs,cjs}': (files) => [
+    `eslint --fix ${files.join(' ')}`,
+    `prettier --write ${files.join(' ')}`,
+  ],
+  '**/*.{json,md,css}': (files) => [`prettier --write ${files.join(' ')}`],
 };
